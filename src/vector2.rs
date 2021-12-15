a5d4e4a955c173053f88a09f314c96954d0d7fea
c1m50c <terminalityms@gmail.com>
2021-12-15 12:48:01 -0600

    Arithmetic Operations for `Vector2` 🧮

--- src/vector2.rs
+++ src/vector2.rs
@@ -1,3 +1,7 @@
+use std::ops::{Add, AddAssign};
+use std::ops::{Sub, SubAssign};
+use std::ops::{Mul, MulAssign};
+use std::ops::{Div, DivAssign};
 use std::cmp::{PartialEq, Eq};
 use std::fmt;
 
@@ -61,6 +65,94 @@ impl<T: PartialEq> PartialEq for Vector2<T> {
 impl<T: Eq> Eq for Vector2<T> {  }
 
 
+impl<T: Add<Output = T>> Add for Vector2<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        return Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        };
+    }
+}
+
+
+impl<T: Add<Output = T> + Copy> AddAssign for Vector2<T> {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        };
+    }
+}
+
+
+impl<T: Sub<Output = T>> Sub for Vector2<T> {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        return Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        };
+    }
+}
+
+
+impl<T: Sub<Output = T> + Copy> SubAssign for Vector2<T> {
+    fn sub_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        };
+    }
+}
+
+
+impl<T: Mul<Output = T>> Mul for Vector2<T> {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        return Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+        };
+    }
+}
+
+
+impl<T: Mul<Output = T> + Copy> MulAssign for Vector2<T> {
+    fn mul_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+        };
+    }
+}
+
+
+impl<T: Div<Output = T>> Div for Vector2<T> {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self::Output {
+        return Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+        };
+    }
+}
+
+
+impl<T: Div<Output = T> + Copy> DivAssign for Vector2<T> {
+    fn div_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+        };
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::Vector2;
@@ -84,5 +176,45 @@ mod tests {
     #[test]
     fn ne() {
         assert_ne!(Vector2::new(0, 1), Vector2::new(0, 0));
+    }
+
+    #[test]
+    fn add() {
+        let v = Vector2::new(0, 1) + Vector2::new(1, 0);
+        assert_eq!(v, Vector2::new(1, 1));
+
+        let mut v = Vector2::new(2, 1);
+        v += Vector2::new(0, 1);
+        assert_eq!(v, Vector2::new(2, 2));
+    }
+
+    #[test]
+    fn sub() {
+        let v = Vector2::new(3, 2) - Vector2::new(1, 1);
+        assert_eq!(v, Vector2::new(2, 1));
+
+        let mut v = Vector2::new(6, 4);
+        v -= Vector2::new(3, 2);
+        assert_eq!(v, Vector2::new(3, 2));
+    }
+
+    #[test]
+    fn mul() {
+        let v = Vector2::new(6, 3) * Vector2::new(2, 2);
+        assert_eq!(v, Vector2::new(12, 6));
+
+        let mut v = Vector2::new(7, 4);
+        v *= Vector2::new(3, 5);
+        assert_eq!(v, Vector2::new(21, 20));
+    }
+
+    #[test]
+    fn div() {
+        let v = Vector2::new(12, 6) / Vector2::new(2, 2);
+        assert_eq!(v, Vector2::new(6, 3));
+
+        let mut v = Vector2::new(32, 50);
+        v /= Vector2::new(4, 25);
+        assert_eq!(v, Vector2::new(8, 2));
     }
 }
