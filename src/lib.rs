e75355dc2f4d20bcb365e7d45939280146ca81de
c1m50c <terminalityms@gmail.com>
2022-01-08 15:48:23 -0600

    `Default` Trait Implementation 🔨

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,30 @@ pub struct Vector3<T> {
 }
 
 
+pub struct Vector4<T> {
+    pub x: T,
+    pub y: T,
+    pub z: T,
+    pub w: T,
+}
+
+
 macro_rules! impl_vector {
-    ($Vector:ident { $($field:ident), + }) => {
+    ($Vector:ident { $($field:ident), + }, $size:expr) => {
         impl<T> $Vector<T> {
+            #[inline]
             pub const fn new($($field: T), +) -> $Vector<T> {
                 return $Vector {
                     $($field: $field), +
+                };
+            }
+        }
+
+        impl<T: Default> Default for $Vector<T> {
+            #[inline]
+            fn default() -> $Vector<T> {
+                return $Vector {
+                    $($field: T::default()), +
                 };
             }
         }
@@ -28,5 +46,6 @@ macro_rules! impl_vector {
 }
 
 
-impl_vector!(Vector2 { x, y });
-impl_vector!(Vector3 { x, y, z });
+impl_vector!(Vector2 { x, y }, 2);
+impl_vector!(Vector3 { x, y, z }, 3);
+impl_vector!(Vector4 { x, y, z, w }, 4);
--- src/tests.rs
+++ src/tests.rs
@@ -5,6 +5,7 @@ use super::*;
 fn new() {
     let vec2 = Vector2::new(0, 1);
     let vec3 = Vector3::new(0, 1, 2);
+    let vec4 = Vector4::new(0, 1, 2, 3);
 
     assert_eq!(vec2.x, 0);
     assert_eq!(vec2.y, 1);
@@ -12,4 +13,29 @@ fn new() {
     assert_eq!(vec3.x, 0);
     assert_eq!(vec3.y, 1);
     assert_eq!(vec3.z, 2);
+
+    assert_eq!(vec4.x, 0);
+    assert_eq!(vec4.y, 1);
+    assert_eq!(vec4.z, 2);
+    assert_eq!(vec4.w, 3);
+}
+
+
+#[test]
+fn default() {
+    let vec2 = Vector2::<i32>::default();
+    let vec3 = Vector3::<i32>::default();
+    let vec4 = Vector4::<i32>::default();
+
+    assert_eq!(vec2.x, 0);
+    assert_eq!(vec2.y, 0);
+
+    assert_eq!(vec3.x, 0);
+    assert_eq!(vec3.y, 0);
+    assert_eq!(vec3.z, 0);
+
+    assert_eq!(vec4.x, 0);
+    assert_eq!(vec4.y, 0);
+    assert_eq!(vec4.z, 0);
+    assert_eq!(vec4.w, 0);
 }
