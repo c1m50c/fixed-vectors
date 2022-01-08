584926ba460ff25eb317b0a3cea83f552d60309d
c1m50c <terminalityms@gmail.com>
2022-01-08 16:56:41 -0600

    `Clone` + `Copy` Traits for Vectors, Slight Documentation ✔📝

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,27 @@
 mod tests;
 
 
+/// Struct for representing a two-dimensional value.
+/// 
+/// ## Fields
+/// ```rust
+/// pub x: T
+/// pub y: T
+/// ```
 pub struct Vector2<T> {
     pub x: T,
     pub y: T,
 }
 
 
+/// Struct for representing a three-dimensional value.
+/// 
+/// ## Fields
+/// ```rust
+/// pub x: T
+/// pub y: T
+/// pub z: T
+/// ```
 pub struct Vector3<T> {
     pub x: T,
     pub y: T,
@@ -15,6 +30,15 @@ pub struct Vector3<T> {
 }
 
 
+/// Struct for representing a four-dimensional value.
+/// 
+/// ## Fields
+/// ```rust
+/// pub x: T
+/// pub y: T
+/// pub z: T
+/// pub w: T
+/// ```
 pub struct Vector4<T> {
     pub x: T,
     pub y: T,
@@ -28,7 +52,7 @@ macro_rules! impl_vector {
         impl<T> $Vector<T> {
             #[inline]
             pub const fn new($($field: T), +) -> $Vector<T> {
-                return $Vector {
+                return Self {
                     $($field: $field), +
                 };
             }
@@ -37,9 +61,18 @@ macro_rules! impl_vector {
         impl<T: Default> Default for $Vector<T> {
             #[inline]
             fn default() -> $Vector<T> {
-                return $Vector {
+                return Self {
                     $($field: T::default()), +
                 };
+            }
+        }
+
+        impl<T: Copy> Copy for $Vector<T> {  }
+
+        impl<T: Copy> Clone for $Vector<T> {
+            #[inline]
+            fn clone(&self) -> $Vector<T> {
+                return *self;
             }
         }
     }
