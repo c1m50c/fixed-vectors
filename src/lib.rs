644c3a8d1a349a0b5277a9369d3410197563f161
c1m50c <terminalityms@gmail.com>
2022-01-08 18:24:02 -0600

    Arithmetic `core::ops` Traits for `Vector` 🧮✔

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,117 @@
 mod tests;
 
 
+use core::ops::{Add, AddAssign};
+use core::ops::{Sub, SubAssign};
+use core::ops::{Mul, MulAssign};
+use core::ops::{Div, DivAssign};
+
+
+#[macro_export]
+macro_rules! impl_vector {
+    ($Vector:ident { $($field:ident), + }, $size:expr) => {
+        impl<T> $Vector<T> {
+            #[inline]
+            pub const fn new($($field: T), +) -> $Vector<T> {
+                return Self {
+                    $($field: $field), +
+                };
+            }
+        }
+
+        impl<T: Default> Default for $Vector<T> {
+            #[inline]
+            fn default() -> $Vector<T> {
+                return Self {
+                    $($field: T::default()), +
+                };
+            }
+        }
+
+        impl<T: Copy> Copy for $Vector<T> {  }
+
+        impl<T: Copy> Clone for $Vector<T> {
+            #[inline]
+            fn clone(&self) -> $Vector<T> {
+                return *self;
+            }
+        }
+
+        impl<T: Add<Output = T>> Add for $Vector<T> {
+            type Output = Self;
+
+            fn add(self, other: Self) -> Self::Output {
+                return Self {
+                    $($field: self.$field + other.$field), +
+                };
+            }
+        }
+
+        impl<T: Add<Output = T> + Copy> AddAssign for $Vector<T> {
+            fn add_assign(&mut self, other: Self) {
+                *self = Self {
+                    $($field: self.$field + other.$field), +
+                };
+            }
+        }
+
+        impl<T: Sub<Output = T>> Sub for $Vector<T> {
+            type Output = Self;
+
+            fn sub(self, other: Self) -> Self::Output {
+                return Self {
+                    $($field: self.$field - other.$field), +
+                };
+            }
+        }
+
+        impl<T: Sub<Output = T> + Copy> SubAssign for $Vector<T> {
+            fn sub_assign(&mut self, other: Self) {
+                *self = Self {
+                    $($field: self.$field - other.$field), +
+                };
+            }
+        }
+
+        impl<T: Mul<Output = T>> Mul for $Vector<T> {
+            type Output = Self;
+
+            fn mul(self, other: Self) -> Self::Output {
+                return Self {
+                    $($field: self.$field * other.$field), +
+                };
+            }
+        }
+
+        impl<T: Mul<Output = T> + Copy> MulAssign for $Vector<T> {
+            fn mul_assign(&mut self, other: Self) {
+                *self = Self {
+                    $($field: self.$field * other.$field), +
+                };
+            }
+        }
+
+        impl<T: Div<Output = T>> Div for $Vector<T> {
+            type Output = Self;
+
+            fn div(self, other: Self) -> Self::Output {
+                return Self {
+                    $($field: self.$field / other.$field), +
+                };
+            }
+        }
+
+        impl<T: Div<Output = T> + Copy> DivAssign for $Vector<T> {
+            fn div_assign(&mut self, other: Self) {
+                *self = Self {
+                    $($field: self.$field / other.$field), +
+                };
+            }
+        }
+    }
+}
+
+
 /// Struct for representing a two-dimensional value.
 /// 
 /// ## Fields
@@ -44,38 +155,6 @@ pub struct Vector4<T> {
     pub y: T,
     pub z: T,
     pub w: T,
-}
-
-
-macro_rules! impl_vector {
-    ($Vector:ident { $($field:ident), + }, $size:expr) => {
-        impl<T> $Vector<T> {
-            #[inline]
-            pub const fn new($($field: T), +) -> $Vector<T> {
-                return Self {
-                    $($field: $field), +
-                };
-            }
-        }
-
-        impl<T: Default> Default for $Vector<T> {
-            #[inline]
-            fn default() -> $Vector<T> {
-                return Self {
-                    $($field: T::default()), +
-                };
-            }
-        }
-
-        impl<T: Copy> Copy for $Vector<T> {  }
-
-        impl<T: Copy> Clone for $Vector<T> {
-            #[inline]
-            fn clone(&self) -> $Vector<T> {
-                return *self;
-            }
-        }
-    }
 }
 
 
--- src/tests.rs
+++ src/tests.rs
@@ -39,3 +39,119 @@ fn default() {
     assert_eq!(vec4.z, 0);
     assert_eq!(vec4.w, 0);
 }
+
+
+#[test]
+fn add() {
+    // TODO: Replace Arithmetic Tests with a macro
+    let mut vec2 = Vector2::<i32>::default();
+    let mut vec3 = Vector3::<i32>::default();
+    let mut vec4 = Vector4::<i32>::default();
+
+    vec2 = vec2 + Vector2::new(1, 2);
+    vec3 = vec3 + Vector3::new(1, 2, 3);
+    vec4 = vec4 + Vector4::new(1, 2, 3, 4);
+
+    vec2 += Vector2::new(1, 2);
+    vec3 += Vector3::new(1, 2, 3);
+    vec4 += Vector4::new(1, 2, 3, 4);
+
+    assert_eq!(vec2.x, 2);
+    assert_eq!(vec2.y, 4);
+
+    assert_eq!(vec3.x, 2);
+    assert_eq!(vec3.y, 4);
+    assert_eq!(vec3.z, 6);
+
+    assert_eq!(vec4.x, 2);
+    assert_eq!(vec4.y, 4);
+    assert_eq!(vec4.z, 6);
+    assert_eq!(vec4.w, 8);
+}
+
+
+#[test]
+fn sub() {
+    // TODO: Replace Arithmetic Tests with a macro
+    let mut vec2 = Vector2::<i32>::default();
+    let mut vec3 = Vector3::<i32>::default();
+    let mut vec4 = Vector4::<i32>::default();
+
+    vec2 = vec2 - Vector2::new(1, 2);
+    vec3 = vec3 - Vector3::new(1, 2, 3);
+    vec4 = vec4 - Vector4::new(1, 2, 3, 4);
+
+    vec2 -= Vector2::new(1, 2);
+    vec3 -= Vector3::new(1, 2, 3);
+    vec4 -= Vector4::new(1, 2, 3, 4);
+
+    assert_eq!(vec2.x, -2);
+    assert_eq!(vec2.y, -4);
+
+    assert_eq!(vec3.x, -2);
+    assert_eq!(vec3.y, -4);
+    assert_eq!(vec3.z, -6);
+
+    assert_eq!(vec4.x, -2);
+    assert_eq!(vec4.y, -4);
+    assert_eq!(vec4.z, -6);
+    assert_eq!(vec4.w, -8);
+}
+
+
+#[test]
+fn mul() {
+    // TODO: Replace Arithmetic Tests with a macro
+    let mut vec2 = Vector2::<i32>::default();
+    let mut vec3 = Vector3::<i32>::default();
+    let mut vec4 = Vector4::<i32>::default();
+
+    vec2 = vec2 * Vector2::new(1, 2);
+    vec3 = vec3 * Vector3::new(1, 2, 3);
+    vec4 = vec4 * Vector4::new(1, 2, 3, 4);
+
+    vec2 *= Vector2::new(1, 2);
+    vec3 *= Vector3::new(1, 2, 3);
+    vec4 *= Vector4::new(1, 2, 3, 4);
+
+    assert_eq!(vec2.x, 0);
+    assert_eq!(vec2.y, 0);
+
+    assert_eq!(vec3.x, 0);
+    assert_eq!(vec3.y, 0);
+    assert_eq!(vec3.z, 0);
+
+    assert_eq!(vec4.x, 0);
+    assert_eq!(vec4.y, 0);
+    assert_eq!(vec4.z, 0);
+    assert_eq!(vec4.w, 0);
+}
+
+
+#[test]
+fn div() {
+    // TODO: Replace Arithmetic Tests with a macro
+    let mut vec2 = Vector2::<i32>::default();
+    let mut vec3 = Vector3::<i32>::default();
+    let mut vec4 = Vector4::<i32>::default();
+
+    vec2 = vec2 / Vector2::new(1, 2);
+    vec3 = vec3 / Vector3::new(1, 2, 3);
+    vec4 = vec4 / Vector4::new(1, 2, 3, 4);
+
+    vec2 /= Vector2::new(1, 2);
+    vec3 /= Vector3::new(1, 2, 3);
+    vec4 /= Vector4::new(1, 2, 3, 4);
+
+    assert_eq!(vec2.x, 0);
+    assert_eq!(vec2.y, 0);
+
+    assert_eq!(vec3.x, 0);
+    assert_eq!(vec3.y, 0);
+    assert_eq!(vec3.z, 0);
+
+    assert_eq!(vec4.x, 0);
+    assert_eq!(vec4.y, 0);
+    assert_eq!(vec4.z, 0);
+    assert_eq!(vec4.w, 0);
+}
