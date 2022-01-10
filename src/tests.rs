1b46d1ca6db2879b763264a61337885686847e12
c1m50c <terminalityms@gmail.com>
2022-01-09 19:24:49 -0600

    `Vector` `Default` Trait Test Cleanup 🧼🧽🧹

--- src/tests.rs
+++ src/tests.rs
@@ -27,17 +27,9 @@ fn default() {
     let vec3 = Vector3::<i32>::default();
     let vec4 = Vector4::<i32>::default();
 
-    assert_eq!(vec2.x, 0);
-    assert_eq!(vec2.y, 0);
-
-    assert_eq!(vec3.x, 0);
-    assert_eq!(vec3.y, 0);
-    assert_eq!(vec3.z, 0);
-
-    assert_eq!(vec4.x, 0);
-    assert_eq!(vec4.y, 0);
-    assert_eq!(vec4.z, 0);
-    assert_eq!(vec4.w, 0);
+    assert_eq!(vec2, Vector2::new(0, 0));
+    assert_eq!(vec3, Vector3::new(0, 0, 0));
+    assert_eq!(vec4, Vector4::new(0, 0, 0, 0));
 }
 
 
