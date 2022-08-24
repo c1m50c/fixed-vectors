4607d5c9ccaf4d4841efda5087cf8ec1dd63c083
c1m50c <58411864+c1m50c@users.noreply.github.com>
2022-08-23 19:08:52 -0500

    more intricate floating point operations

--- src/macros/floating.rs
+++ src/macros/floating.rs
@@ -67,6 +67,56 @@ macro_rules! impl_floating_vector {
                     $( $field: self.$field + (weight * (to.$field - self.$field)) ), +
                 };
             }
+
+            fn normalized(self) -> Self {
+                let mut iter = Self {
+                    $( $field: self.$field * self.$field ), +
+                }.into_iter();
+
+                let mut magnitude = iter.next()
+                    .expect("Cannot normalize a Vector that contains no fields.");
+                iter.for_each(|float| magnitude = magnitude + float);
+
+                if magnitude == T::zero() {
+                    return self.zero();
+                }
+
+                let magnitude = magnitude.sqrt();
+                return Self {
+                    $( $field: self.$field / magnitude ), +
+                };
+            }
+
+            fn dot(self, b: Self) -> T {
+                let mut iter = Self {
+                    $( $field: self.$field * b.$field ), +
+                }.into_iter();
+
+                let mut dot_product = iter.next()
+                    .expect("Cannot normalize a Vector that contains no fields.");
+                iter.for_each(|float| dot_product = dot_product * float);
+                return dot_product;
+            }
+
+            fn length_squared(self) -> T {
+                let mut iter = Self {
+                    $( $field: self.$field * self.$field ), +
+                }.into_iter();
+
+                let mut added = iter.next()
+                    .expect("Cannot normalize a Vector that contains no fields.");
+                iter.for_each(|float| added = added + float);
+                return added;
+            }
+
+            fn length(self) -> T {
+                return self.length_squared().sqrt();
+            }
+
+            fn direction(self, to: Self) -> Self {
+                todo!("Need to implement arithmetic traits first.");
+                // return (to - self).normalized();
+            }
         }
     }
 }
