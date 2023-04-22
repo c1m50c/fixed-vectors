8d7366e49f011f8bfd3f66140080d52bc1886a10
c1m50c <58411864+c1m50c@users.noreply.github.com>
2023-04-22 17:42:05 -0500

    3.x.x: `no_std` version (#3)
    * 3.x.x: `no_std` version
    
    * Update GitHub Actions
    
    * Fix Clippy Error
    
    * Fix `secrets.*`
    
    * Remove needless `cargo login`
    
    * Floating Point Operations Impl
    
    * Floating Point Operations Documentation
    
    * Fix clippy warning
    
    * Finishing Touches
    
    * Fix path because weirdness

--- src/lib.rs
+++ src/lib.rs
@@ -1,125 +1,75 @@
-//! Library containing a collection of fixed-size Vectors.
-//! These Vectors are useful for use in positional, and dimensional values.
-//! There are three built-in Vectors to choose from, the built-in Vectors are the structs
-//! [`Vector2`], [`Vector3`], and [`Vector4`].
-//! 
-//! # Example
-//! ```rust
-//! use fixed_vectors::Vector3;
-//! 
-//! let vec = Vector3::new(1, 2, 3);
-//! let sum = vec.into_iter().sum::<i32>();
-//! assert_eq!(sum, 6);
-//! ```
+#![doc = include_str!("../README.MD")]
+#![no_std]
+
+mod macros;
 
 #[cfg(test)]
 mod tests;
 
-pub mod macros;
 
-/// The type returned in [`Result`]s created by Vector functions.
+/// Vector for holding two-dimensional values.
 /// 
 /// # Example
-/// ```rust
-/// use fixed_vectors::{Vector2, VectorError};
-/// use std::convert::TryFrom;
 /// 
-/// // This returns an `Err` because the given `Vec` is not as big as a `Vector2`
-/// let try_from_vec = Vector2::try_from(vec![1]);
-/// 
-/// if let Err(error) = try_from_vec {
-///     assert_eq!(error, VectorError::CannotConvertFromImproperlySizedCollection);
-/// }
 /// ```
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub enum VectorError {
-    /// Returned when there's not enough information to give a more verbose [`VectorError`].
-    GenericError,
-
-    /// Given when converting a Dynamically Sized Collection into a Vector,
-    /// only when the Collection's size is less than the Vector's.
-    /// 
-    /// # Example
-    /// ```rust
-    /// use fixed_vectors::{Vector3, VectorError};
-    /// use std::convert::TryFrom;
-    /// 
-    /// let vec = vec![1, 2];
-    /// let res = Vector3::try_from(vec);
-    /// assert_eq!(res.err().unwrap(), VectorError::CannotConvertFromImproperlySizedCollection);
-    /// ```
-    CannotConvertFromImproperlySizedCollection
-}
-
-
-/// A Vector for holding two dimensional-values.
-/// 
-/// # Example
-/// ```rust
 /// use fixed_vectors::Vector2;
 /// 
-/// let vec2 = Vector2::new(1, 2);
-/// assert_eq!(vec2.x, 1);
-/// assert_eq!(vec2.y, 2);
+/// let mut vec2 = Vector2::new(1, 2);
+/// vec2 += Vector2::new(1, 2);
+/// 
+/// assert_eq!(vec2.x, 2);
+/// assert_eq!(vec2.y, 4);
 /// ```
 pub struct Vector2<T> {
-    /// Field holding the first dimensional-value.
     pub x: T,
-
-    /// Field holding the second dimensional-value.
     pub y: T,
 }
 
 
-/// A Vector for holding three dimensional-values.
+/// Vector for holding three-dimensional values.
 /// 
 /// # Example
-/// ```rust
+/// 
+/// ```
 /// use fixed_vectors::Vector3;
 /// 
-/// let vec3 = Vector3::new(1, 2, 3);
-/// assert_eq!(vec3.x, 1);
-/// assert_eq!(vec3.y, 2);
-/// assert_eq!(vec3.z, 3);
+/// let mut vec3 = Vector3::new(1, 2, 3);
+/// vec3 += Vector3::new(1, 2, 3);
+/// 
+/// assert_eq!(vec3.x, 2);
+/// assert_eq!(vec3.y, 4);
+/// assert_eq!(vec3.z, 6);
 /// ```
 pub struct Vector3<T> {
-    /// Field holding the first dimensional-value.
     pub x: T,
-
-    /// Field holding the second dimensional-value.
     pub y: T,
-
-    /// Field holding the third dimensional-value.
     pub z: T,
 }
 
 
-/// A Vector for holding four dimensional-values.
+/// Vector for holding four-dimensional values.
 /// 
 /// # Example
-/// ```rust
+/// 
+/// ```
 /// use fixed_vectors::Vector4;
 /// 
-/// let vec4 = Vector4::new(1, 2, 3, 4);
-/// assert_eq!(vec4.x, 1);
-/// assert_eq!(vec4.y, 2);
-/// assert_eq!(vec4.z, 3);
-/// assert_eq!(vec4.w, 4);
+/// let mut vec4 = Vector4::new(1, 2, 3, 4);
+/// vec4 += Vector4::new(1, 2, 3, 4);
+/// 
+/// assert_eq!(vec4.x, 2);
+/// assert_eq!(vec4.y, 4);
+/// assert_eq!(vec4.z, 6);
+/// assert_eq!(vec4.w, 8);
 /// ```
 pub struct Vector4<T> {
-    /// Field holding the first dimensional-value.
     pub x: T,
-
-    /// Field holding the second dimensional-value.
     pub y: T,
-
-    /// Field holding the third dimensional-value.
     pub z: T,
-
-    /// Field holding the fourth dimensional-value.
     pub w: T,
 }
 
-impl_vector!(Vector2 { x, y } -> (T, T), 2);
-impl_vector!(Vector3 { x, y, z } -> (T, T, T), 3);
-impl_vector!(Vector4 { x, y, z, w } -> (T, T, T, T), 4);
+
+impl_vector!(Vector2 { x, y }, 2);
+impl_vector!(Vector3 { x, y, z }, 3);
+impl_vector!(Vector4 { x, y, z, w }, 4);
--- src/macros/floating.rs
+++ src/macros/floating.rs
@@ -1,269 +1,315 @@
-/// Macro used in implementing all the floating-point methods for Vectors.
-/// 
-/// # Example
-/// ```
-/// use fixed_vectors::impl_vector;
-/// 
-/// struct Vector2<T> {
-///     x: T,
-///     y: T,
-/// }
-/// 
-/// impl_vector!(Vector2 { x, y } -> (T, T), 2);
-/// let vec = Vector2::new(1.5, 2.5);
-/// 
-/// assert_eq!(vec.floor(), Vector2::new(1.0, 2.0));
-/// ```
-#[macro_export]
+#[macro_export(local_inner_macros)]
 macro_rules! impl_floating_point_operations {
-    ($struct: ident { $($field: ident), + }, $len: expr) => {
-        #[allow(dead_code)]
-        impl<T: num_traits::Float> $struct<T> {
-            /// Converts all numbers within the Vector to zero.
+    ( $struct: ident { $($field: ident), + }, $size: expr, f32 ) => {
+        impl $struct<f32> {
+            /// Consumes the vector and returns it with all of its fields converted to their square-root as a [`f32`].
             /// 
             /// # Example
-            /// ```rust
+            /// 
+            /// ```
             /// use fixed_vectors::Vector2;
             /// 
-            /// let vector = Vector2::new(1.0, 2.0).zero();
-            /// assert_eq!(vector, Vector2::new(0.0, 0.0));
+            /// let vec2 = Vector2::new(64.0, 25.0).sqrt_short();
+            /// 
+            /// assert_eq!(vec2, Vector2::new(8.0, 5.0));
             /// ```
-            pub fn zero(self) -> Self {
-                /*
-                    TODO: Look into this later?
-                    NOTE: 🤔 I wonder what's more performant:
-                    - Mapping the given Vector with the required functions.
-                    - Constructing a new Vector calling the required function on each field.
-                */
-                return self.map(|_| T::zero());
+            pub fn sqrt_short(self) -> Self {
+                self.map(|f| libm::sqrtf(f))
             }
 
-            /// Converts all numbers within the Vector to largest integer
-            /// less than or equal to the value.
+            /// Returns the magnitude of the vector as a [`f32`].
             /// 
             /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector2;
             /// 
-            /// let vector = Vector2::new(1.25, 5.9).floor();
-            /// assert_eq!(vector, Vector2::new(1.0, 5.0));
             /// ```
-            pub fn floor(self) -> Self {
-                return self.map(|f| f.floor());
+            /// use fixed_vectors::Vector3;
+            /// 
+            /// let vec3 = Vector3::new(1.5, 2.0, 3.33);
+            /// let length = vec3.length_short();
+            /// 
+            /// assert!(length < 4.2);
+            /// ```
+            pub fn length_short(&self) -> f32 {
+                libm::sqrtf(self.length_squared())
             }
 
-            /// Converts all numbers within the Vector to largest integer
-            /// greater than or equal to the value.
+            /// Consumes the vector and returns it as normalized vector of [`f32s`](f32).
             /// 
             /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector2;
             /// 
-            /// let vector = Vector2::new(1.25, 5.9).ceil();
-            /// assert_eq!(vector, Vector2::new(2.0, 6.0));
             /// ```
-            pub fn ceil(self) -> Self {
-                return self.map(|f| f.ceil());
-            }
-
-            /// Converts all numbers within the Vector to the nearest integer.
-            /// 
-            /// # Example
-            /// ```rust
             /// use fixed_vectors::Vector2;
             /// 
-            /// let vector = Vector2::new(1.25, 5.9).round();
-            /// assert_eq!(vector, Vector2::new(1.0, 6.0));
+            /// let vec2 = Vector2::new(14.3, 7.9).normalized_short();
+            /// 
+            /// assert!(vec2.x < 1.0);
+            /// assert!(vec2.y < 1.0);
             /// ```
-            pub fn round(self) -> Self {
-                return self.map(|f| f.round());
-            }
+            pub fn normalized_short(self) -> Self {
+                let length_squared = self.length_squared();
 
-            /// Converts all numbers within the Vector to their absolute value.
+                if length_squared == 0.0 {
+                    return Self { $( $field: 0.0 ), + };
+                }
+
+                let length = libm::sqrtf(length_squared);
+
+                Self {
+                    $( $field: self.$field / length ), +
+                }
+            }
+        }
+    };
+
+    ( $struct: ident { $($field: ident), + }, $size: expr, f64 ) => {
+        impl $struct<f64> {
+            /// Consumes the vector and returns it with all of its fields converted to their square-root as a [`f64`].
             /// 
             /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector2;
             /// 
-            /// let vector = Vector2::new(-1.0, 1.0).abs();
-            /// assert_eq!(vector, Vector2::new(1.0, 1.0));
             /// ```
-            pub fn abs(self) -> Self {
-                return self.map(|f| f.abs());
-            }
-
-            /// Converts all numbers within the Vector to their integer parts.
-            /// 
-            /// # Example
-            /// ```rust
             /// use fixed_vectors::Vector2;
             /// 
-            /// let vector = Vector2::new(1.337, 2.5).trunc();
-            /// assert_eq!(vector, Vector2::new(1.0, 2.0));
-            /// ```
-            pub fn trunc(self) -> Self {
-                return self.map(|f| f.trunc());
-            }
-
-            /// Converts all numbers within the Vector to their fractional parts.
+            /// let vec2 = Vector2::new(64.0, 25.0).sqrt();
             /// 
-            /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector2;
-            /// 
-            /// let vector = Vector2::new(1.337, 2.5).fract();
-            /// assert!(vector.x <= 0.337);
-            /// assert!(vector.y <= 0.5);
-            /// ```
-            pub fn fract(self) -> Self {
-                return self.map(|f| f.fract());
-            }
-
-            /// Converts all numbers within the Vector to their square-root.
-            /// 
-            /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector2;
-            /// 
-            /// let vector = Vector2::new(64.0, 25.0).sqrt();
-            /// assert_eq!(vector, Vector2::new(8.0, 5.0));
+            /// assert_eq!(vec2, Vector2::new(8.0, 5.0));
             /// ```
             pub fn sqrt(self) -> Self {
-                return self.map(|f| f.sqrt());
+                self.map(|f| libm::sqrt(f))
             }
 
-            /// Raises all numbers within the Vector to an integer power.
+            /// Returns the magnitude of the vector as a [`f64`].
             /// 
             /// # Example
-            /// ```rust
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector3;
+            /// 
+            /// let vec3 = Vector3::new(1.5, 2.0, 3.33);
+            /// let length = vec3.length();
+            /// 
+            /// assert!(length < 4.2);
+            /// ```
+            pub fn length(&self) -> f64 {
+                libm::sqrt(self.length_squared())
+            }
+
+            /// Consumes the vector and returns it as normalized vector of [`f64s`](f64).
+            /// 
+            /// # Example
+            /// 
+            /// ```
             /// use fixed_vectors::Vector2;
             /// 
-            /// let vector = Vector2::new(2.0, 4.0).powi(2);
-            /// assert_eq!(vector, Vector2::new(4.0, 16.0));
+            /// let vec2 = Vector2::new(14.3, 7.9).normalized();
+            /// 
+            /// assert!(vec2.x < 1.0);
+            /// assert!(vec2.y < 1.0);
+            /// ```
+            pub fn normalized(self) -> Self {
+                let length_squared = self.length_squared();
+
+                if length_squared == 0.0 {
+                    return Self { $( $field: 0.0 ), + };
+                }
+
+                let length = libm::sqrt(length_squared);
+
+                Self {
+                    $( $field: self.$field / length ), +
+                }
+            }
+        }
+    };
+}
+
+
+#[macro_export(local_inner_macros)]
+macro_rules! impl_cross_type_floating_point_operations {
+    ( $struct: ident { $($field: ident), + }, $size: expr ) => {
+        impl<T: num_traits::float::FloatCore> $struct<T> {
+            /// Returns the dot product of two vectors.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector2;
+            /// 
+            /// let a = Vector2::new(1.0, 2.0);
+            /// let b = Vector2::new(2.0, 4.0);
+            /// let dot = a.dot(&b);
+            /// 
+            /// assert_eq!(dot, 10.0);
+            /// ```
+            #[inline]
+            pub fn dot(&self, other: &Self) -> T {
+                $crate::sum_repeating!(
+                    $( + (self.$field * other.$field) ) +
+                )
+            }
+
+            /// Returns the squared magnitude of vector.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector3;
+            /// 
+            /// let vec3 = Vector3::new(3.33, 2.04, 1.337);
+            /// let lsq = vec3.length_squared();
+            /// 
+            /// assert!(lsq >= 17.0);
+            /// ```
+            pub fn length_squared(&self) -> T {
+                let squared = Self {
+                    $( $field: self.$field * self.$field ), +
+                };
+
+                $crate::sum_repeating!(
+                    $( + squared.$field ) +
+                )
+            }
+
+            /// Applies [`floor`](num_traits::float::FloatCore::floor) on all fields within the vector,
+            /// converting each field to the largest integer less than or equal to its value.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector2;
+            /// 
+            /// let vec2 = Vector2::new(1.6, 2.3).floor();
+            /// 
+            /// assert_eq!(vec2, Vector2::new(1.0, 2.0));
+            /// ```
+            pub fn floor(self) -> Self {
+                self.map(|f| f.floor())
+            }
+
+            /// Applies [`ceil`](num_traits::float::FloatCore::ceil) on all fields within the vector,
+            /// converting each field to the largest integer greater than or equal to its value.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector2;
+            /// 
+            /// let vec2 = Vector2::new(1.6, 2.3).ceil();
+            /// 
+            /// assert_eq!(vec2, Vector2::new(2.0, 3.0));
+            /// ```
+            pub fn ceil(self) -> Self {
+                self.map(|f| f.ceil())
+            }
+
+            /// Applies [`round`](num_traits::float::FloatCore::round) on all fields within the vector,
+            /// converting each field's value to its nearest integer.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector2;
+            /// 
+            /// let vec2 = Vector2::new(1.6, 2.3).round();
+            /// 
+            /// assert_eq!(vec2, Vector2::new(2.0, 2.0));
+            /// ```
+            pub fn round(self) -> Self {
+                self.map(|f| f.round())
+            }
+
+            /// Applies [`abs`](num_traits::float::FloatCore::abs) on all fields within the vector,
+            /// converting each field to their absolute value.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector2;
+            /// 
+            /// let vec2 = Vector2::new(-2.6, 2.3).abs();
+            /// 
+            /// assert_eq!(vec2, Vector2::new(2.6, 2.3));
+            /// ```
+            pub fn abs(self) -> Self {
+                self.map(|f| f.abs())
+            }
+
+            /// Applies [`trunc`](num_traits::float::FloatCore::trunc) on all fields within the vector,
+            /// converting each field's value to their integer parts.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector2;
+            /// 
+            /// let vec2 = Vector2::new(-2.6, 2.3).trunc();
+            /// 
+            /// assert_eq!(vec2, Vector2::new(-2.0, 2.0));
+            /// ```
+            pub fn trunc(self) -> Self {
+                self.map(|f| f.trunc())
+            }
+
+            /// Applies [`fract`](num_traits::float::FloatCore::fract) on all fields within the vector,
+            /// converting each field's value to their fractional parts.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector2;
+            /// 
+            /// let vec2 = Vector2::new(-2.5, 2.25).fract();
+            /// 
+            /// assert_eq!(vec2, Vector2::new(-0.5, 0.25));
+            /// ```
+            pub fn fract(self) -> Self {
+                self.map(|f| f.fract())
+            }
+
+            /// Applies [`powi`](num_traits::float::FloatCore::powi) on all fields within the vector,
+            /// raising each field's value to an integer power.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector2;
+            /// 
+            /// let vec2 = Vector2::new(2.0, 4.0).powi(2);
+            /// 
+            /// assert_eq!(vec2, Vector2::new(4.0, 16.0));
             /// ```
             pub fn powi(self, n: i32) -> Self {
-                return self.map(|f| f.powi(n));
-            }
-
-            /// Raises all numbers within the Vector to a floating-point power.
-            /// 
-            /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector2;
-            /// 
-            /// let vector = Vector2::new(2.0, 4.0).powf(2.0);
-            /// assert_eq!(vector, Vector2::new(4.0, 16.0));
-            /// ```
-            pub fn powf(self, n: T) -> Self {
-                return self.map(|f| f.powf(n));
+                self.map(|f| f.powi(n))
             }
 
             /// Linearly interpolates between two Vectors by a normalized `weight`.
             /// 
             /// # Example
-            /// ```rust
+            /// 
+            /// ```
             /// use fixed_vectors::Vector2;
             /// 
-            /// let vector = Vector2::new(1.0, 2.0).lerp(Vector2::new(2.0, 3.0), 1.0);
-            /// assert_eq!(vector, Vector2::new(2.0, 3.0));
+            /// let vec2 = Vector2::new(1.0, 2.0).lerp(
+            ///     Vector2::new(2.0, 3.0), 1.0
+            /// );
+            /// 
+            /// assert_eq!(vec2, Vector2::new(2.0, 3.0));
             /// ```
             pub fn lerp(self, to: Self, weight: T) -> Self {
-                return Self {
+                Self {
                     $( $field: self.$field + (weight * (to.$field - self.$field)) ), +
-                };
-            }
-
-            /// Normalizes the Vector.
-            /// 
-            /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector2;
-            /// 
-            /// let vector = Vector2::new(14.3, 7.9).normalized();
-            /// assert!(vector.x < 1.0);
-            /// assert!(vector.y < 1.0);
-            /// ```
-            pub fn normalized(self) -> Self {
-                let mut iter = self.clone().into_iter()
-                    .map(|f| f * f);
-
-                let start = iter.next()
-                    .expect("Cannot normalize a Vector that contains no fields.");
-                let mut magnitude = iter.fold(start, |acc, f| acc * f);
-
-                if magnitude == T::zero() { return self.zero(); }
-                magnitude = magnitude.sqrt();
-
-                return Self {
-                    $( $field: self.$field / magnitude ), +
-                };
-            }
-
-            /// Returns the dot product of two Vectors,
-            /// this can be used to compare the angle between two Vectors.
-            /// 
-            /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector2;
-            /// 
-            /// let dot = Vector2::new(1.0, 2.0).dot(Vector2::new(2.0, 4.0));
-            /// assert_eq!(dot, 16.0);
-            /// ```
-            pub fn dot(self, b: Self) -> T {
-                let mut iter = self.into_iter()
-                    .zip(b.into_iter())
-                    .map(|(a, b)| a * b);
-
-                let start = iter.next()
-                    .expect("Cannot retrieve the dot product of a Vector that contains no fields.");
-                return iter.fold(start, |acc, f| acc * f);
-            }
-
-            /// Returns the squared magnitude of the Vector.
-            /// This will always run faster than [`length`] and should be prefered over it if applicable.
-            /// 
-            /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector3;
-            /// 
-            /// let lsq = Vector3::new(3.33, 2.04, 1.337).length_squared();
-            /// assert!(lsq >= 17.0);
-            /// ```
-            pub fn length_squared(self) -> T {
-                return self.into_iter()
-                    .map(|f| f * f)
-                    .fold(T::zero(), |acc, f| acc + f);
-            }
-
-            /// Returns the magnitude of the Vector.
-            /// 
-            /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector3;
-            /// 
-            /// let len = Vector3::new(1.5, 2.0, 3.33).length();
-            /// assert!(len < 4.2);
-            /// ```
-            pub fn length(self) -> T {
-                return self.length_squared().sqrt();
-            }
-
-            /// Returns a normalized Vector pointing from it to `to`.
-            /// 
-            /// # Example
-            /// ```rust
-            /// use fixed_vectors::Vector2;
-            /// 
-            /// let from = Vector2::new(1.0, 2.0);
-            /// let to = Vector2::new(5.0, 6.0);
-            /// 
-            /// let dir = from.direction(to);
-            /// assert!(dir.x < 1.0);
-            /// assert!(dir.y < 1.0);
-            /// ```
-            pub fn direction(self, to: Self) -> Self {
-                return (to - self).normalized();
+                }
             }
         }
-    }
+    };
+}
+
+
+// HACK: Allows us to sum repeating tokens in macros.
+// See: https://stackoverflow.com/a/60187870/17452730
+#[macro_export(local_inner_macros)]
+macro_rules! sum_repeating {
+    ( + $($item: tt) * ) => {
+        $($item) *
+    };
 }
