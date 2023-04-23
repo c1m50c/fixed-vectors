7851aa76e8333ab066ca87a17e2a6da63f87b829
c1m50c <58411864+c1m50c@users.noreply.github.com>
2023-04-23 12:06:32 -0500

    Version 3.2.1 (#6)
    * Refactoring Float Method Names
    
    * Version Bump

--- src/macros/floating.rs
+++ src/macros/floating.rs
@@ -1,133 +1,6 @@
 #[doc(hidden)]
 #[macro_export(local_inner_macros)]
 macro_rules! impl_floating_point_operations {
-    ( $struct: ident { $($field: ident), + }, $size: expr, f32 ) => {
-        impl $struct<f32> {
-            /// Consumes the vector and returns it with all of its fields converted to their square-root as a [`f32`].
-            /// 
-            /// # Example
-            /// 
-            /// ```
-            /// use fixed_vectors::Vector2;
-            /// 
-            /// let vec2 = Vector2::new(64.0, 25.0).sqrt_short();
-            /// 
-            /// assert_eq!(vec2, Vector2::new(8.0, 5.0));
-            /// ```
-            pub fn sqrt_short(self) -> Self {
-                self.map(|f| libm::sqrtf(f))
-            }
-
-            /// Returns the magnitude of the vector as a [`f32`].
-            /// 
-            /// # Example
-            /// 
-            /// ```
-            /// use fixed_vectors::Vector3;
-            /// 
-            /// let vec3 = Vector3::new(1.5, 2.0, 3.33);
-            /// let length = vec3.length_short();
-            /// 
-            /// assert!(length < 4.2);
-            /// ```
-            pub fn length_short(&self) -> f32 {
-                libm::sqrtf(self.length_squared())
-            }
-
-            /// Consumes the vector and returns it as normalized vector of [`f32s`](f32).
-            /// 
-            /// # Example
-            /// 
-            /// ```
-            /// use fixed_vectors::Vector2;
-            /// 
-            /// let vec2 = Vector2::new(14.3, 7.9).normalized_short();
-            /// 
-            /// assert!(vec2.x < 1.0);
-            /// assert!(vec2.y < 1.0);
-            /// ```
-            pub fn normalized_short(self) -> Self {
-                let length_squared = self.length_squared();
-
-                if length_squared == 0.0 {
-                    return Self { $( $field: 0.0 ), + };
-                }
-
-                let length = libm::sqrtf(length_squared);
-
-                Self {
-                    $( $field: self.$field / length ), +
-                }
-            }
-        }
-    };
-
-    ( $struct: ident { $($field: ident), + }, $size: expr, f64 ) => {
-        impl $struct<f64> {
-            /// Consumes the vector and returns it with all of its fields converted to their square-root as a [`f64`].
-            /// 
-            /// # Example
-            /// 
-            /// ```
-            /// use fixed_vectors::Vector2;
-            /// 
-            /// let vec2 = Vector2::new(64.0, 25.0).sqrt();
-            /// 
-            /// assert_eq!(vec2, Vector2::new(8.0, 5.0));
-            /// ```
-            pub fn sqrt(self) -> Self {
-                self.map(|f| libm::sqrt(f))
-            }
-
-            /// Returns the magnitude of the vector as a [`f64`].
-            /// 
-            /// # Example
-            /// 
-            /// ```
-            /// use fixed_vectors::Vector3;
-            /// 
-            /// let vec3 = Vector3::new(1.5, 2.0, 3.33);
-            /// let length = vec3.length();
-            /// 
-            /// assert!(length < 4.2);
-            /// ```
-            pub fn length(&self) -> f64 {
-                libm::sqrt(self.length_squared())
-            }
-
-            /// Consumes the vector and returns it as normalized vector of [`f64s`](f64).
-            /// 
-            /// # Example
-            /// 
-            /// ```
-            /// use fixed_vectors::Vector2;
-            /// 
-            /// let vec2 = Vector2::new(14.3, 7.9).normalized();
-            /// 
-            /// assert!(vec2.x < 1.0);
-            /// assert!(vec2.y < 1.0);
-            /// ```
-            pub fn normalized(self) -> Self {
-                let length_squared = self.length_squared();
-
-                if length_squared == 0.0 {
-                    return Self { $( $field: 0.0 ), + };
-                }
-
-                let length = libm::sqrt(length_squared);
-
-                Self {
-                    $( $field: self.$field / length ), +
-                }
-            }
-        }
-    };
-}
-
-
-#[doc(hidden)]
-#[macro_export(local_inner_macros)]
-macro_rules! impl_cross_type_floating_point_operations {
     ( $struct: ident { $($field: ident), + }, $size: expr ) => {
         impl<T: num_traits::float::FloatCore> $struct<T> {
             /// Returns the dot product of two vectors.
@@ -184,8 +57,9 @@ macro_rules! impl_cross_type_floating_point_operations {
             /// 
             /// assert_eq!(vec2, Vector2::new(1.0, 2.0));
             /// ```
+            #[inline]
             pub fn floor(self) -> Self {
-                self.map(|f| f.floor())
+                self.map(T::floor)
             }
 
             /// Applies [`ceil`](num_traits::float::FloatCore::ceil) on all fields within the vector,
@@ -200,8 +74,9 @@ macro_rules! impl_cross_type_floating_point_operations {
             /// 
             /// assert_eq!(vec2, Vector2::new(2.0, 3.0));
             /// ```
+            #[inline]
             pub fn ceil(self) -> Self {
-                self.map(|f| f.ceil())
+                self.map(T::ceil)
             }
 
             /// Applies [`round`](num_traits::float::FloatCore::round) on all fields within the vector,
@@ -216,8 +91,9 @@ macro_rules! impl_cross_type_floating_point_operations {
             /// 
             /// assert_eq!(vec2, Vector2::new(2.0, 2.0));
             /// ```
+            #[inline]
             pub fn round(self) -> Self {
-                self.map(|f| f.round())
+                self.map(T::round)
             }
 
             /// Applies [`abs`](num_traits::float::FloatCore::abs) on all fields within the vector,
@@ -232,8 +108,9 @@ macro_rules! impl_cross_type_floating_point_operations {
             /// 
             /// assert_eq!(vec2, Vector2::new(2.6, 2.3));
             /// ```
+            #[inline]
             pub fn abs(self) -> Self {
-                self.map(|f| f.abs())
+                self.map(T::abs)
             }
 
             /// Applies [`trunc`](num_traits::float::FloatCore::trunc) on all fields within the vector,
@@ -248,8 +125,9 @@ macro_rules! impl_cross_type_floating_point_operations {
             /// 
             /// assert_eq!(vec2, Vector2::new(-2.0, 2.0));
             /// ```
+            #[inline]
             pub fn trunc(self) -> Self {
-                self.map(|f| f.trunc())
+                self.map(T::trunc)
             }
 
             /// Applies [`fract`](num_traits::float::FloatCore::fract) on all fields within the vector,
@@ -264,8 +142,9 @@ macro_rules! impl_cross_type_floating_point_operations {
             /// 
             /// assert_eq!(vec2, Vector2::new(-0.5, 0.25));
             /// ```
+            #[inline]
             pub fn fract(self) -> Self {
-                self.map(|f| f.fract())
+                self.map(T::fract)
             }
 
             /// Applies [`powi`](num_traits::float::FloatCore::powi) on all fields within the vector,
@@ -280,6 +159,7 @@ macro_rules! impl_cross_type_floating_point_operations {
             /// 
             /// assert_eq!(vec2, Vector2::new(4.0, 16.0));
             /// ```
+            #[inline]
             pub fn powi(self, n: i32) -> Self {
                 self.map(|f| f.powi(n))
             }
@@ -297,9 +177,75 @@ macro_rules! impl_cross_type_floating_point_operations {
             /// 
             /// assert_eq!(vec2, Vector2::new(2.0, 3.0));
             /// ```
+            #[inline(always)]
             pub fn lerp(self, to: Self, weight: T) -> Self {
                 Self {
                     $( $field: self.$field + (weight * (to.$field - self.$field)) ), +
+                }
+            }
+        }
+
+        impl<T> $struct<T>
+        where
+            T: $crate::macros::floating::_FloatingPoint
+            + num_traits::float::FloatCore
+        {
+            /// Consumes the vector and returns it with all of its fields converted to their square-root.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector2;
+            /// 
+            /// let vec2 = Vector2::new(64.0, 25.0).sqrt();
+            /// 
+            /// assert_eq!(vec2, Vector2::new(8.0, 5.0));
+            /// ```
+            #[inline]
+            pub fn sqrt(self) -> Self {
+                self.map(T::sqrt)
+            }
+
+            /// Returns the magnitude of the vector.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector3;
+            /// 
+            /// let vec3 = Vector3::new(1.5, 2.0, 3.33);
+            /// let length = vec3.length();
+            /// 
+            /// assert!(length < 4.2);
+            /// ```
+            #[inline]
+            pub fn length(&self) -> T {
+                self.length_squared().sqrt()
+            }
+
+            /// Consumes the vector and returns it as normalized vector.
+            /// 
+            /// # Example
+            /// 
+            /// ```
+            /// use fixed_vectors::Vector2;
+            /// 
+            /// let vec2 = Vector2::new(14.3, 7.9).normalized();
+            /// 
+            /// assert!(vec2.x < 1.0);
+            /// assert!(vec2.y < 1.0);
+            /// ```
+            pub fn normalized(self) -> Self {
+                let length_squared = self.length_squared();
+
+                if length_squared == T::zero() {
+                    return Self { $( $field: T::zero() ), + };
+                }
+
+                let length = length_squared.sqrt();
+
+                Self {
+                    $( $field: self.$field / length ), +
                 }
             }
         }
@@ -309,9 +255,33 @@ macro_rules! impl_cross_type_floating_point_operations {
 
 // HACK: Allows us to sum repeating tokens in macros.
 // See: https://stackoverflow.com/a/60187870/17452730
+#[doc(hidden)]
 #[macro_export(local_inner_macros)]
 macro_rules! sum_repeating {
     ( + $($item: tt) * ) => {
         $($item) *
     };
+}
+
+
+// Required trait for `sqrt` impls
+#[doc(hidden)]
+pub trait _FloatingPoint {
+    fn sqrt(self) -> Self;
+}
+
+
+impl _FloatingPoint for f32 {
+    #[inline(always)]
+    fn sqrt(self) -> Self {
+        libm::sqrtf(self)
+    }
+}
+
+
+impl _FloatingPoint for f64 {
+    #[inline(always)]
+    fn sqrt(self) -> Self {
+        libm::sqrt(self)
+    }
 }
